2d9332c8009720ce2b2cee60f60f89da3b04e725
CL Wu <CharlieWu500@gmail.com>
2022-07-08 23:39:00 +0800

    v1.0 new repo!

---
+++ bootstrap_skrollr/assets/js/script.js
@@ -0,0 +1,51 @@
+var ctx = document.getElementById('pieChart');
+
+pieChart = new Chart(ctx, {
+  type: 'pie', // 圖表類型
+  data: {
+    labels: ['React', 'Angular', 'Vue'], // 標題
+    datasets: [
+      {
+        data: [74, 53, 24], // 資料
+        backgroundColor: [
+          // 背景色
+          '#8ADBEB',
+          '#EB9D97',
+          '#ABE87D',
+        ],
+        borderWidth: 1, // 外框寬度
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        position: 'top',
+        reverse: 'true',
+      },
+    },
+  },
+});
+
+$(window).scroll(function (evt) {
+  let navbarH = $('.navbar').outerHeight();
+  let x = innerHeight - navbarH;
+  if ($(window).scrollTop() > x) $('.navbar').addClass('nav-white');
+  else $('.navbar').removeClass('nav-white');
+});
+
+$('#more_essay').hide();
+skrollr.init();
+document.body.id = 'skrollr-body';
+
+$('.more_button').click(function () {
+  if ($('.more_button').hasClass('active')) {
+    $('.more_button').removeClass('active');
+    $('.more_button').html('其他');
+    $('#more_essay').slideUp();
+  } else {
+    $('.more_button').addClass('active');
+    $('.more_button').html('收回');
+    $('#more_essay').slideDown();
+  }
+});
